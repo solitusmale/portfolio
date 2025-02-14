b380dde28a1f2b1114234c90e3df1cc1215eae10
Aleksa <alexaw32@hotmail.com>
2025-02-14 12:25:47 +0100

    slike2icons

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,9 +1,10 @@
 import {RiReactjsLine} from "react-icons/ri";
-import {TbBrandNextjs} from "react-icons/tb";
+import { TbBrandPython } from "react-icons/tb";
 import {SiMongodb} from "react-icons/si";
-import {DiRedis} from "react-icons/di";
+import { FaWordpress } from "react-icons/fa";
 import {FaNodeJs} from "react-icons/fa";
-import {BiLogoPostgresql} from "react-icons/bi";
+import { TbBrandVite } from "react-icons/tb";
+
 import { animate, motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
@@ -44,7 +45,7 @@ const Technologies = () => {
             initial="initial"
             animate="animate"
             className="rounded-2xl border-4 border-neutral-800 p-4">
-                <TbBrandNextjs className="text-7xl text-cyan-400"/>
+                <TbBrandPython className="text-7xl text-cyan-400"/>
             </motion.div>
             <motion.div 
             variants={iconVariants(5)}
@@ -58,7 +59,7 @@ const Technologies = () => {
             initial="initial"
             animate="animate"
             className="rounded-2xl border-4 border-neutral-800 p-4">
-                <DiRedis className="text-7xl text-red-700"/>
+                <FaWordpress className="text-7xl text-red-700"/>
             </motion.div>
             <motion.div 
             variants={iconVariants(6)}
@@ -72,7 +73,7 @@ const Technologies = () => {
             initial="initial"
             animate="animate"
             className="rounded-2xl border-4 border-neutral-800 p-4">
-                <BiLogoPostgresql className="text-7xl text-sky-700"/>
+                <TbBrandVite className="text-7xl text-sky-700"/>
             </motion.div>
         </motion.div>
         </div>
